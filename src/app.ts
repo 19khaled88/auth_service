a5160c15d948fd470ea216abbc2954e59bddc48f
19khaled88 <kkhaled88hasan@gmail.com>
2023-10-11 12:24:53 +0600

    eslint established

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
-import express, { Application, Request, Response } from  'express'
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
+//application
+const app: Application = express()
 
-//application 
-const app:Application = express()
+app.use(cors())
 
+//parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-
-app.get('/',(req:Request,res:Response)=>{
-    res.send('Hello word')
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello word')
 })
 
 export default app
